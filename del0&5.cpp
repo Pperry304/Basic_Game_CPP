141003ae99dd43b6685fa65a988675ef7bcfe382
Trường Chinh <156447398+TruongChinh304@users.noreply.github.com>
2024-11-19 17:02:22 +0700

    Create del0&5.cpp

---
+++ del0&5.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <cstdlib> // random
+#include <unistd.h> // time delay (sleep)
+#define __TruongChinh__ signed main()
+#define loop while (true)
+#define ll long long
+#define pb push_back
+#define FOR(i, a, b) for(int i = a; i <= b; i++)
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define SNT(i, a, n) for(int i = 2; i*i <= n; i++)
+#define fast_io ios::sync_with_stdio(0); cin.tie(0);
+#define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+using namespace std;
+
+__TruongChinh__ {
+    fast_io;
+    
+    string s; 
+    getline(cin, s);
+    for(char c : s){
+        if(c != '0' && c != '5'){
+            cout << c;
+        }
+    }
+
+    cerr << "Time: " << TIME << " s.\n";
+    return 0;
+}
