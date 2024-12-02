f97b835c744129e681871185c5cd4ec237c6f2ea
Trường Chinh <156447398+TruongChinh304@users.noreply.github.com>
2024-12-02 21:13:47 +0700

    Update Pair.cpp

--- Pair.cpp
+++ Pair.cpp
@@ -1,19 +1,26 @@
 #include <bits/stdc++.h>
-#include <cstdlib> // random
-#include <unistd.h> // time delay (sleep)
-#define __TruongChinh__ signed main()
+#define ___TruongChinh304___ signed main()
 #define loop while (true)
 #define ll long long
+#define ull unsigned long long
 #define pb push_back
+#define endl '\n'
+#define ONLINE_JUDGE
+#define ALl(x) x.begin(), x.end()
 #define FOR(i, a, b) for(int i = a; i <= b; i++)
+#define FOD(i, a, b) for(int i = a; i >= b; i--)
 #define REP(i, n) for(int i = 0; i < n; i++)
 #define SNT(i, a, n) for(int i = 2; i*i <= n; i++)
 #define fast_io ios::sync_with_stdio(0); cin.tie(0);
 #define TIME (1.0 * clock() / CLOCKS_PER_SEC)
+
 using namespace std;
 
-__TruongChinh__ {
-    fast_io;
+
+___TruongChinh304___ {
+    #ifdef ONLINE_JUDGE
+    fast_io;  
+    #endif
     
     int n; cin >> n;
     vector<int> a(n);
